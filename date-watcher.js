b6697898c903cff1dcb8f0b52721aa4d5146d2ba
AKM-Studios <92059298+AKM-Studios@users.noreply.github.com>
2022-10-05 14:41:39 -0400

    Create date-watcher.js

---
+++ date-watcher.js
@@ -0,0 +1,32 @@
+/*
+To use this script, do the following:
+	1. log in to https://ais.usvisa-info.com
+	2. Click on the "Continue" button to go to the actions page
+	3. Open up Console on your browser
+	4. Copy and paste this script into the console and press Enter to run the script
+*/
+
+
+'use strict';
+
+// Enter the desired date (or earlier) in yyyy-mm-dd format
+var desiredDate = new Date('2023-02-01');
+
+var path = window.location.pathname,
+	id = path.split('/')[4];
+
+var scrapeInterval = setInterval(function () {
+	$.getJSON('https://ais.usvisa-info.com/en-ca/niv/schedule/' + id + '/appointment/days/94.json?appointments[expedite]=false', function (data) {
+		if(data.length === 0) {
+		console.log('Not available');
+		}
+		if(data.length > 0) {
+		var earliestDate = new Date(data[0].date);
+		if (earliestDate.getTime() <= desiredDate.getTime()) {
+			window.alert('Earliest date available: ' + earliestDate.toDateString());
+		} else {
+			console.log('No date available');
+		}
+	}
+	});
+}, 10000);
